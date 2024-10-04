21b18ebe4b7be7dc2f944104ed5f4cf37e8da85b
Milan T <tresnakmilan1@gmail.com>
2024-10-04 18:35:43 +0200

    rezervace v tride main
    - vytvoreny list na rezervace

--- src/Main.java
+++ src/Main.java
@@ -11,24 +11,31 @@ public class Main {
     Guest jan = new Guest("Jan ","Dvořáček ",
                 LocalDate.of(1995, 5, 5));
     jan.setBirthdate(LocalDate.of(1955, 4, 5));
+
         System.out.println(jan.getName() + jan.getLastName() + "(" +
                     jan.getBirthdate() + ").");
         System.out.println(adela.getName() + adela.getLastName() + "(" +
                 adela.getBirthdate() + ").");
 
-        Rooms n1 = new Rooms("č.1 ", 2,
+        Rooms n1 = new Rooms("č.1 ", 1,
                 true, true,
                 BigDecimal.valueOf(1000));
-        Rooms n2 = new Rooms("č.2 ", 2,
+        Rooms n2 = new Rooms("č.2 ", 1,
                 true, true,
                 BigDecimal.valueOf(1000));
         Rooms n3 = new Rooms("č.3 ", 3,
                 false, true,
                 BigDecimal.valueOf(2400));
-
+        
         BookingManager room1 = new BookingManager(
                 "od 19. do 26.7.2021 ", false);
         BookingManager room3 = new BookingManager(
-                "3 od 1. do 14. 9. 2021", true);
+                "od 1. do 14. 9. 2021", true);
+
+        List<BookingManager> reservation = new ArrayList<>();
+        reservation.add(room1);
+        reservation.add(room3);
+        System.out.println(reservation.getFirst().getReservation());
+        System.out.println(reservation.getLast().getReservation());
     }
 }
