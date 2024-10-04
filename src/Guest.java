5f06f50ee940fc53f5f6b3e083051cb143c22c9f
Milan T <tresnakmilan1@gmail.com>
2024-10-04 17:03:29 +0200

    vytvorene tridy

--- src/Guest.java
+++ src/Guest.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class guest {
+public class Guest {
+    private String name;
+    private String lastName;
+    private LocalDate birthdate;
+
+    public Guest(String name, String lastName, LocalDate birthdate) {
+        this.name = name;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
 }
--- src/Rooms.java
+++ src/Rooms.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.math.BigDecimal;
 
 public class Rooms {
+    private int roomNumber;
+    private int numberOgBeds;
+    boolean withBalcony;
+    boolean coastView;
+    BigDecimal price;
+
+    public Rooms(int roomNumber, int numberOgBeds, boolean withBalcony, boolean coastView, BigDecimal price) {
+        this.roomNumber = roomNumber;
+        this.numberOgBeds = numberOgBeds;
+        this.withBalcony = withBalcony;
+        this.coastView = coastView;
+        this.price = price;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getNumberOgBeds() {
+        return numberOgBeds;
+    }
+
+    public void setNumberOgBeds(int numberOgBeds) {
+        this.numberOgBeds = numberOgBeds;
+    }
+
+    public boolean isWithBalcony() {
+        return withBalcony;
+    }
+
+    public void setWithBalcony(boolean withBalcony) {
+        this.withBalcony = withBalcony;
+    }
+
+    public boolean isCoastView() {
+        return coastView;
+    }
+
+    public void setCoastView(boolean coastView) {
+        this.coastView = coastView;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
