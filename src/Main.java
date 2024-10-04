49b44ef581d9517cb318f4ba596a0130770f6c57
Milan T <tresnakmilan1@gmail.com>
2024-10-04 17:14:02 +0200

    vytvoreny kod v metode main
    - kod nam vytvoril 2 hosty
    - kod nadale vypsal udaje o hostech

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+    Guest adela = new Guest("Adéla ", "Malíková ",
+                LocalDate.of(1993,3, 13));
+    Guest jan = new Guest("Jan ","Dvořáček ",
+                LocalDate.of(1995, 5, 5));
+    jan.setBirthdate(LocalDate.of(1955, 4, 5));
+        System.out.println(jan.getName() + jan.getLastName() + "(" +
+                    jan.getBirthdate() + ").");
+        System.out.println(adela.getName() + adela.getLastName() + "(" +
+                adela.getBirthdate() + ").");
 
 
     }
