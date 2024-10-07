c55e25aad096018cac92d025e0a99eff6fa5a09d
Milan T <tresnakmilan1@gmail.com>
2024-10-07 15:09:47 +0200

    prirazeni atributu name a room
    - dotaz

--- src/Booking.java
+++ src/Booking.java
@@ -34,4 +34,17 @@ public class Booking {
                 "pro hosta jménem: " + guest.getName() + guest.getLastName() +
                 otherGuest.getName() + otherGuest.getLastName();
     }
+
+    /* pridani otherGuest pres "List" jak jsi psal.... jsem nepochopl
+    *  jak to mam dat cele dohromady, aby otherGuest byl jen u nektere rezervace...
+    *  zkousel jsem to pres metodu "add", ale když jsem to pak chtel vypsat, v tomto
+    *  "String toString", tak mi to neslo jelikoz nektere rezervace nemeli "otherGuest"
+    *  prirazene.
+    *
+    *  pridal jsem tedy "otherGuest" tak to jak vidis, ale stale mi to nejde vypsat, jelikoz
+    *  nektere rezervace maji ten atribut a nektere ne... tak hadam, ze se to bude muset
+    *  vypsat pomoci metody if else.
+    *
+    *  ale taky ocekavam ze to je uplne spatne, jen jestli se to da resit i takto pres
+    *  "setOtherGuest"?*/
 }
